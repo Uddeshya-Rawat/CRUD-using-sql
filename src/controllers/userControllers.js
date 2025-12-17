585011b15b9f57e6b03ed75f6be2199a9afc5793
Uddeshya Rawat <uddeshyarawat4@gmail.com>
2025-12-17 22:44:19 +0530

    created update ,delete , showAllUser api and updated code here in userService , userModel, userRoutes,userCOntrollers

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -3,7 +3,7 @@ const {userServices}=require('../services')
 console.log('inside user controllers')
 
 async function createUser(req,res){
-    console.log(req.body)
+    console.log("this is data comming from front end ",req.body)
     
     try {
         const response = await userServices.createUser(req.body)
@@ -33,7 +33,65 @@ async function getUsers(req,res){
     }
 }
 
+async function getSingleUser(req,res){
+    
+    try {
+        const response=await userServices.getSingleUser(req.params.id)
+        console.log("this is single user response",response)
+        if(!response){
+            return res.status(300).json({
+                error:true,
+                message:'something went wrong or id didnt matched '
+            })
+        }
+        return res.status(200).json({
+            error:false,
+            data:response,
+        })
+    } catch (error) {
+        return error
+    }
+}
+
+async function updateUser(req,res){
+    
+    try {
+        const response = await userServices.updateUser(req.params.id,req.body)
+         if(!response){
+            return res.status(400).json({
+                message:"soemthing went wrong or id is incorrect"
+            })
+         }
+         return res.status(200).json({
+            error:false,
+            message:"user updated successfully"
+         })
+    } catch (error) {
+        return error
+    }
+}
+
+async function deleteUser(req,res){
+    try {
+        const response = await userServices.deleteUser(req.params.id)
+        if(!response){
+            return res.status(400).json({
+                message:"soemthing went wrong or id is incorrect"
+            })
+         }
+         return res.status(200).json({
+            error:false,
+            message:"user deleted successfully"
+         })
+    } catch (error) {
+        return error 
+    }
+}
+
+
 module.exports={
 createUser,
-getUsers
+getUsers,
+getSingleUser,
+updateUser,deleteUser
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,5 +5,8 @@ const router=express.Router()
 
 router.post('/createUser',userControllers.createUser)
 router.get('/getAllUsers',userControllers.getUsers)
+router.patch('/updateUser/:id',userControllers.updateUser)
+router.delete('/deleteUser/:id',userControllers.deleteUser)
+router.get('/getUserById/:id',userControllers.getSingleUser)
 
 module.exports = router
